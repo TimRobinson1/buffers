82f210928dc60fb7101d5a7eb656b824b8baf85c
Timothy Robinson <timmyrobby@gmail.com>
2017-07-27 07:26:00 +0100

    Adds separate menu views

--- app/app.rb
+++ app/app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   erb(:index)
 end
+
+get '/menus/bistro' do
+  erb(:bistro)
+end
+
+get '/menus/main' do
+  erb(:day_menu)
+end
+
+get '/contact' do
+  erb(:contact)
+end
